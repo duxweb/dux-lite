dd83e19d60a1122db44fbca14e645c94b7227e10
duxphp <admin@duxphp.com>
2023-11-01 21:21:07 +0800

    Optimize cache config

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Dux\Cache;
 
 use Dux\App;
+use Phpfastcache\CacheManager;
 use Phpfastcache\Config\ConfigurationOption;
 use Phpfastcache\Helper\Psr16Adapter;
 
@@ -12,20 +13,14 @@ class Cache
 
     public static function init(string $type): Psr16Adapter
     {
-        $driverConfig = [];
-        if ($type != 'files') {
-            $driver = App::config('cache')->get('driver', 'default');
-            $driverConfig = App::config('database')->get($type . ".drivers." . $driver);
+        if ($type === "files") {
+            $config = [];
+            $config["path"] = App::$dataPath . "/cache";
         }
-
-        $config = match ($type) {
-            'redis' => $driverConfig,
-            'files' => [
-                'path' => App::$dataPath . "/cache"
-            ]
-        };
-
-
+        if ($type === "redis") {
+            $driver = App::config('cache')->get('driver', 'default');
+            $config = App::config('database')->get($type . ".drivers." . $driver);
+        }
         return new Psr16Adapter($type, new ConfigurationOption($config));
     }
 }
