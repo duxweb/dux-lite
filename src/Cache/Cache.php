e68db62b4d27c7bde694e006c614b13e39fb3054
duxphp <admin@duxphp.com>
2023-11-01 21:28:29 +0800

    Optimize cache config

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -6,6 +6,7 @@ namespace Dux\Cache;
 use Dux\App;
 use Phpfastcache\CacheManager;
 use Phpfastcache\Config\ConfigurationOption;
+use Phpfastcache\Drivers\Redis\Config;
 use Phpfastcache\Helper\Psr16Adapter;
 
 class Cache
@@ -14,13 +15,22 @@ class Cache
     public static function init(string $type): Psr16Adapter
     {
         if ($type === "files") {
-            $config = [];
-            $config["path"] = App::$dataPath . "/cache";
+            $config = new ConfigurationOption([
+                'path' => App::$dataPath . "/cache"
+            ]);
         }
         if ($type === "redis") {
             $driver = App::config('cache')->get('driver', 'default');
             $config = App::config('database')->get($type . ".drivers." . $driver);
+            $config = new Config([
+                'host' => $config['host'],
+                'port' => $config['port'],
+                'timeout' => (int) $config['timeout'],
+                'password' => $config['auth'] ?: '',
+                'database' => (int) $config['database'] ?: 0,
+                'optPrefix' => $config['optPrefix'] ?: '',
+            ]);
         }
-        return new Psr16Adapter($type, new ConfigurationOption($config));
+        return new Psr16Adapter($type, $config);
     }
 }
