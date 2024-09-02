bc8678f02c12540b8d65420198e06c7b9ee7a28c
ts <757496835@qq.com>
2024-09-02 16:50:35 +0800

    fast: 修改队列脚本默认值

--- src/Queue/QueueCommand.php
+++ src/Queue/QueueCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class QueueCommand extends Command
@@ -21,8 +20,9 @@ class QueueCommand extends Command
     {
         $this->addArgument(
             'name',
-            InputArgument::REQUIRED,
-            'please enter the queue name'
+            InputArgument::OPTIONAL,
+            'please enter the queue name',
+            'queue'
         );
     }
 
