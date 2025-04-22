c1836498910a8d195d5b2dd80983f577f88b6596
Mr.L <admin@duxphp.com>
2025-04-22 14:32:43 +0800

    更新密码

--- src/Database/Redis/PhpRedisAdapter.php
+++ src/Database/Redis/PhpRedisAdapter.php
@@ -10,7 +10,7 @@ class PhpRedisAdapter
         $this->client = new \Redis();
         $this->client->connect($options["host"] ?? '127.0.0.1', (int)$options["port"] ?? 6379, (float)$options["timeout"]);
         if ($options["password"]) {
-            $this->client->auth($options["auth"]);
+            $this->client->auth($options["password"]);
         }
         $database = (int)$options["database"] ?: 0;
         $this->client->select($database);
