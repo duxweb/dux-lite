9bd33a46b8f5ca133530fdf7425cd88a5948a664
Mr.L <admin@duxphp.com>
2025-07-08 21:11:07 +0800

    在 Route 属性类中添加 app 参数支持，并将所有参数改为公共属性以提高访问便捷性，同时优化构造函数参数顺序和注释。

--- src/Route/Attribute/Route.php
+++ src/Route/Attribute/Route.php
@@ -13,13 +13,15 @@ class Route
      * @param array|string $methods 请求方法
      * @param string $route 路由匹配
      * @param string $name 路由名称
+     * @param string|null $app 路由注册名
      * @param bool $auth 是否需要登录
      */
     public function __construct(
-        array|string $methods,
-        string       $route,
-        string       $name = '',
-        bool         $auth = true,
+        public array|string $methods,
+        public string       $route,
+        public string       $name = '',
+        public ?string      $app = null,
+        public bool         $auth = true,
     )
     {
     }
