74e9c1737774d8b295376a7aab2b173ef96078f8
Mr.L <admin@duxphp.com>
2026-03-02 23:17:15 +0800

    refactor(SchedulerCommand): 移除命令行选项和文件监控功能
    移除了 watch-interval 命令行选项以及相关的文件变化监控逻辑，
    简化了调度器命令的执行流程。不再支持通过 PCNTL 进行进程分叉
    和文件监控功能，使命令更加简洁稳定。

--- src/Scheduler/SchedulerCommand.php
+++ src/Scheduler/SchedulerCommand.php
@@ -7,7 +7,6 @@ use Core\App;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SchedulerCommand extends Command
@@ -15,15 +14,11 @@ class SchedulerCommand extends Command
     protected function configure(): void
     {
         $this->setName("scheduler:run")->setDescription('Scheduler start service');
-        $this
-            ->addOption('watch-interval', null, InputOption::VALUE_REQUIRED, 'Watch interval (seconds)', '3');
     }
 
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $watchInterval = max(1, (int)$input->getOption('watch-interval'));
-
         $scheduler = App::scheduler();
         $jobs = $scheduler->loadJobs();
         $output->writeln('<info>Watching jobs file: ' . $scheduler->jobsFilePath() . '</info>');
@@ -39,45 +34,7 @@ class SchedulerCommand extends Command
             ->setRows($rows);
         $table->render();
 
-        if (function_exists('pcntl_fork') && function_exists('posix_kill')) {
-            $jobsPath = $scheduler->jobsFilePath();
-            $hash = $this->hashFile($jobsPath);
-            $pid = pcntl_fork();
-            if ($pid === -1) {
-                $output->writeln('<error>Unable to fork watcher process, running without watcher.</error>');
-            } elseif ($pid === 0) {
-                $code = $scheduler->run();
-                exit($code === Scheduler::EXIT_OK ? Command::SUCCESS : $code);
-            } else {
-                $interval = max(1, $watchInterval);
-                while (true) {
-                    $status = null;
-                    $res = pcntl_waitpid($pid, $status, WNOHANG);
-                    if ($res === $pid) {
-                        $exitCode = pcntl_wexitstatus($status);
-                        return $exitCode === 0 ? Command::SUCCESS : $exitCode;
-                    }
-
-                    sleep($interval);
-                    $newHash = $this->hashFile($jobsPath);
-                    if ($hash !== $newHash) {
-                        $output->writeln('<comment>Jobs file changed, stopping scheduler...</comment>');
-                        App::log('scheduler')->info('Scheduler jobs file changed, stopping', [
-                            'file' => $jobsPath,
-                        ]);
-                        posix_kill($pid, SIGTERM);
-                        sleep(1);
-                        posix_kill($pid, SIGKILL);
-                        return Scheduler::EXIT_RESTART;
-                    }
-                }
-            }
-        }
-
         $code = $scheduler->run();
-        if ($code === Scheduler::EXIT_RESTART) {
-            return Scheduler::EXIT_RESTART;
-        }
         return $code === Scheduler::EXIT_OK ? Command::SUCCESS : $code;
     }
 
@@ -95,12 +52,4 @@ class SchedulerCommand extends Command
         return $rows;
     }
 
-    private function hashFile(string $path): ?string
-    {
-        clearstatcache(true, $path);
-        if (!is_file($path)) {
-            return null;
-        }
-        return hash_file('sha256', $path) ?: null;
-    }
 }
